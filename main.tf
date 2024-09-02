c88167f84318363d6d5e9d794a919ff4bc7455ca
vinaykumar2233 <vk6709016@gmail.com>
2024-09-02 21:05:21 +0530

    first commit2

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "vinay"
+  location = "Westus"
+}
